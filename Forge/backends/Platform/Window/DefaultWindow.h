017127135b8964cd9603cabfbd2ebab9707298c7
Alexei1R <rusu.alexei.2002@gmail.com>
2025-01-21 15:01:27 +0200

    change: copyright

--- Forge/backends/Platform/Window/DefaultWindow.h
+++ Forge/backends/Platform/Window/DefaultWindow.h
@@ -1,5 +1,5 @@
 
-// Copyright (c) 2025-present, Rusu Alexei & Forge contributors.
+// Copyright (c) 2025-present, Rusu Alexei & Project contributors.
 // Distributed under the MIT License (http://opensource.org/licenses/MIT)
 
 #ifndef DEFAULTWINDOW_H
--- Forge/include/Forge/Utils/Common.h
+++ Forge/include/Forge/Utils/Common.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2025-present, Rusu Alexei & Forge contributors.
+// Copyright (c) 2025-present, Rusu Alexei & Project contributors.
 // Distributed under the MIT License (http://opensource.org/licenses/MIT)
 
 #ifndef COMMON_H
